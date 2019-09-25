d0026c37e706a7c5e627556ccb2e5b2995e627fe
MtnDewritos <seb@gieser.se>
2019-09-25 13:13:25 +0200

    ai code
    unity froze completely and broke the touchpad drivers in the process but here's the code anyway exdedede

--- Assets/scripts/Ai.cs
+++ Assets/scripts/Ai.cs
@@ -4,15 +4,91 @@ using UnityEngine;
 
 public class Ai : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> nodes = new List<GameObject>();
+    private float nextMove = 0;
+    private float timeInterval = 3f;
+    private int currentNode = 0;
+    private float rayDist = 10f;
+    private float viewDist = 6f;
+    private float speed = 3f;
+    //private float chaseSpeed; //kanske ge spelaren en sprint också
+
+    private int layerMask = 8;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Look();
+        float time = Time.time;
+        if(time > nextMove)
+        {
+            MoveToNextNode();
+            nextMove = time + timeInterval;
+        }
+                     
+    }
+    private void MoveToNode()
+    {
+        Vector3 position = nodes[currentNode].transform.position;
+        while (transform.position.normalized != position.normalized)
+        {
+            if(transform.position.normalized.x != position.normalized.x)
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            else if (transform.position.normalized.z != position.normalized.z)
+            {
+                transform.Translate(0, 0, speed * Time.deltaTime);
+            }            
+        }
+        transform.Rotate(nodes[currentNode].transform.rotation.eulerAngles);
+
+    }
+    private void MoveToNextNode()
+    {
+        MoveToNode();
+        currentNode++;
+    }
+    private void Chase(Vector3 position)
+    {
+        while (transform.position.normalized != position.normalized)
+        {
+            if (transform.position.normalized.x != position.normalized.x)
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            else if (transform.position.normalized.z != position.normalized.z)
+            {
+                transform.Translate(0, 0, speed * Time.deltaTime);
+            }
+        }
         
+    }
+    private void Return()
+    {
+
+    }
+    private void Look()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward));
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayDist, layerMask))
+        {
+            Debug.Log("raycast hit");
+            if(hit.distance < viewDist)
+            {
+                Debug.Log("hit within view distance");
+                Vector3 position = hit.collider.transform.position;
+
+                Chase(position);
+            }
+            
+        }
+       
     }
 }
